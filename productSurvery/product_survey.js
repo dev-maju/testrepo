31256918c0e8a3fbb5289b05761bc120d241103d
dev-maju <majuprojects2025@gmail.com>
2026-02-15 12:47:51 -0500

    added user experience box

--- productSurvery/product_survey.js
+++ productSurvery/product_survey.js
@@ -10,6 +10,7 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('userExperience').value;
 
     
     alert('Thank you for your valuable feedback');
@@ -20,6 +21,7 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('displayExperience').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 }
 
