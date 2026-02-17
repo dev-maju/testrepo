999a32047ecbb81640be3b28593ed0e2ef0c66da
dev-maju <majuprojects2025@gmail.com>
2026-02-17 09:22:56 -0500

    added Quotient and Sum to the code

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,19 +1,24 @@
     
     function performOperation() {
     // Get user input from input fields
-    let num1 = parseInt(document.getElementById('input1').value);
-    let num2 = parseInt(document.getElementById('input2').value);
-    // Check if inputs are valid numbers
-    if (!isNaN(num1) && !isNaN(num2)) {
-    // Perform the operation
-                    let result = multiply(num1, num2);
+        let num1 = parseInt(document.getElementById('input1').value);
+        let num2 = parseInt(document.getElementById('input2').value);
+        // Check if inputs are valid numbers
+        if (!isNaN(num1) && !isNaN(num2)) {
+        // Perform the operation
+            let mul = multiply(num1, num2);
+            let sum = add(num1, num2);
+            let div = division(num1,num2);
 
-                    // Display the result
-                    displayResult(result);
-                } else {
-                    displayResult('Please enter valid numbers');
-                }
-            }
+            displayResult(`
+                Product = ${mul}\n
+                Quotient = ${div}\n
+                Sum = ${sum}
+            `);
+        } else {
+            displayResult('Please enter valid numbers');
+        }
+    }
 
             function multiply(a, b) {
                 // Introduce a debugger statement to pause execution
@@ -23,9 +28,22 @@
                 return a * b;
             }
 
+
+            function add(a,b){
+                debugger;
+                return a+b;
+            }
+
+            function division(a,b){
+                debugger;
+                return a/b;
+            }
+
+            
             function displayResult(result) {
                 // Display the result in the paragraph element
                 const resultElement = document.getElementById('result');
                 resultElement.textContent = `The result is: ${result}`;
             }
+
         
